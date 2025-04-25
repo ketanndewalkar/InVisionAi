e4758f262e046073f8836cb0cfd5d2c43e590466
Ketan <dewalkarketan123@gmail.com>
2025-04-25 18:31:35 +0530

    final commit

---
+++ src/app/layout.js
@@ -0,0 +1,21 @@
+// "use client"
+
+import "./globals.css";
+import UiProvider from "./components/UiProvider";
+
+import StoreProvider from "./components/StoreProvider";
+import { Toaster } from "react-hot-toast";
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`antialiased`}>
+        <UiProvider>
+          <StoreProvider>{children}</StoreProvider>
+          <Toaster/>
+          
+        </UiProvider>
+        {/* {children} */}
+      </body>
+    </html>
+  );
+}
